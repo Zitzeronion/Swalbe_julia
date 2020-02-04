63a753654e3a832cbb4995b22a57cf8a1825a5be
Stefan Zitz <s.zitz@fz-juelich.de>
2020-02-04 16:08:11 +0100

    Feature: D1Q3 moments calculation added

--- Scripts/macroquantities.jl
+++ Scripts/macroquantities.jl
@@ -7,13 +7,15 @@ Theoretically this quantities are the moments of the distribution function.
 
 # D2Q9 lattice velocity vector: (0,0) (1,0) (0,1) ....
 Cd2q9 = [0 1 0 -1 0 1 -1 -1 1; 0 0 1 0 -1 1 1 -1 -1]
+# D1Q3 lattice velocity
+Cd1q3 = [0 1 -1]
 
 
 """
-    momentsd2q9!(height, velocity_x, velocity_y, distribution)
+    momentsd2q9(height, velocity_x, velocity_y, distribution)
 
 Computes the macroscopic quantities `height` and `velocity` from a `distribution function`.
-See also: [`equiliriumd2q9`](@ref), [`equiliriumd1q3`](@ref).
+See also: [`momentsguod2q9`](@ref), [`momentsd1q3`](@ref).
 
 # Arguments
 - `height::Array{Float64,2}` : `height`-value at every lattice node, similar to density in standard lattice Boltzmann
@@ -23,16 +25,16 @@ See also: [`equiliriumd2q9`](@ref), [`equiliriumd1q3`](@ref).
 
 # Examples
 ```jldoctest
-julia> A = [1 1; 1 1]; 
-julia> B = [0 0; 0 0];                  # example without velocity
+julia> A = [1.0 1.0; 1.0 1.0]; 
+julia> B = [0.0 0.0; 0.0 0.0];                  # example without velocity
 julia> D = fill(1.0, (9, 2, 2))
-julia> C = momentsd2q9!(A,B,B,D); 
-julia> println(C[1, :, :])
-[1.0 1.0; 1.0 1.0]
+julia> h_out,velx_out,vely_out = momentsd2q9(A,B,B,D); 
+julia> println(h_out)
+[9.0 9.0; 9.0 9.0]
 ```
 
 """
-function momentsd2q9!(height, velocity_x, velocity_y, distribution)
+function momentsd2q9(height, velocity_x, velocity_y, distribution)
     # Get the size for the loops
     lat_vel, rows, cols = size(distribution)
 
@@ -44,14 +46,62 @@ function momentsd2q9!(height, velocity_x, velocity_y, distribution)
     for node_x = 1:rows
         for node_y = 1:cols
             for q = 1:lat_vel
-                sumresult[node_x, node_y] += distribution[q, node_x, node_y]                       # Sum up for the height, zeroth moment
-                sumvelx[node_x, node_y] += Cd2q9[1,q] .* distribution[q, node_x, node_y]     # First moment 
-                sumvely[node_x, node_y] += Cd2q9[2,q] .* distribution[q, node_x, node_y]     # Second moment
+                sumresult[node_x, node_y] += distribution[q, node_x, node_y]                # Sum up for the height, zeroth moment
+                sumvelx[node_x, node_y] += Cd2q9[1,q] * distribution[q, node_x, node_y]     # First moment 
+                sumvely[node_x, node_y] += Cd2q9[2,q] * distribution[q, node_x, node_y]     # Second moment
             end
         end
     end
-
+    # Normalize the values according to standard LBM steps
     height = sumresult
     velocity_x = sumvelx./sumresult
     velocity_y = sumvely./sumresult
+    # Return the computed values for the
+    return height, velocity_x, velocity_y
+end
+
+
+"""
+    momentsd1q3(height, velocity, distribution)
+
+Computes the macroscopic quantities `height` and `velocity` from a `distribution function`.
+See also: [`momentsd2q9`](@ref), [`momentsguod2q9`](@ref).
+
+# Arguments
+- `height::Array{Float64,1}` : `height`-value at every lattice node, similar to density in standard lattice Boltzmann
+- `velocity::Array{Float64,1}` : The macroscopic `velocity` at every lattice node
+- `distribution::Array{Float64,2}` : Internal distribution function used for the lattice Boltzmann algorithm
+
+# Examples
+```jldoctest
+julia> A = [1.0 1.0 1.0 1.0]; 
+julia> B = [0.0 0.0 0.0 0.0];                  # example without velocity
+julia> D = fill(1.0, (3, 4))
+julia> h_out,vel_out = momentsd2q9(A,B,D); 
+julia> println(h_out)
+[3.0 3.0 3.0 3.0]
+```
+
+"""
+function momentsd1q3(height, velocity, distribution)
+    # Get the size for the loops
+    lat_vel, rows = size(distribution)
+
+    # Intermediate array to store the result
+    sumresult = zeros(rows)
+    sumvel = zeros(rows)
+
+    # apparently Julia is fast when it comes to loops
+    for node_x = 1:rows
+        for q = 1:lat_vel
+            sumresult[node_x] += distribution[q, node_x]                # Sum up for the height, zeroth moment
+            sumvel[node_x] += Cd1q3[q] * distribution[q, node_x]        # First moment 
+        end
+    end
+    # Normalize the values according to standard LBM steps
+    height = sumresult
+    velocity = sumvel./sumresult
+    
+    # Return the computed values for the
+    return height, velocity
 end
