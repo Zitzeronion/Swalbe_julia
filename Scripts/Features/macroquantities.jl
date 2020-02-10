42c73accaa23134fd354316be8fa8d35938febaf
Stefan Zitz <s.zitz@fz-juelich.de>
2020-02-10 13:58:47 +0100

    Feature: Guo Forcing for momentsd2q9 -> momentsguod2q9

--- Scripts/Features/macroquantities.jl
+++ Scripts/Features/macroquantities.jl
@@ -55,6 +55,59 @@ function momentsd2q9(height, velocity_x, velocity_y, distribution)
     return height, velocity_x, velocity_y
 end
 
+"""
+    momentsguod2q9(height, velocity_x, velocity_y, forces_x, forces_y, distribution)
+
+Computes the macroscopic quantities `height` and `velocity` from a `distribution function` and the applied `forces`.
+See also: [`momentsd2q9`](@ref), [`momentsd1q3`](@ref).
+
+# Arguments
+- `height::Array{Float64,2}` : `height`-value at every lattice node, similar to density in standard lattice Boltzmann
+- `velocity_x::Array{Float64,2}` : The `x`-component of the macroscopic velocity
+- `velocity_y::Array{Float64,2}` : The `y`-component of the macroscopic velocity
+- `forces_x::Array{Float64,2}` : The `x`-component of summed up forces
+- `forces_y::Array{Float64,2}` : The `y`-component of summed up forces
+- `distribution::Array{Float64,3}` : Internal distribution function used for the lattice Boltzmann algorithm
+
+# Examples
+```jldoctest
+julia> A = [1.0 1.0; 1.0 1.0]; 
+julia> B = [0.0 0.0; 0.0 0.0];                  # example without velocity
+julia> Fx = [1.0 1.0; 1.0 1.0];                 # And way to much forcing
+julia> Fy = [0.0 0.0; 0.0 0.0];
+julia> D = fill(1.0, (9, 2, 2))
+julia> h_out,velx_out,vely_out = momentsd2q9(A,B,B,Fx,Fy,D); 
+julia> println(h_out)  
+[9.0 9.0; 9.0 9.0]
+```
+
+"""
+function momentsguod2q9(height, velocity_x, velocity_y, forces_x, forces_y, distribution)
+    # Get the size for the loops
+    lat_vel, rows, cols = size(distribution)
+
+    # Intermediate array to store the result
+    sumresult = zeros(rows, cols)
+    sumvelx = zeros(rows, cols)
+    sumvely = zeros(rows, cols)
+    # apparently Julia is fast when it comes to loops
+    for node_x = 1:rows
+        for node_y = 1:cols
+            for q = 1:lat_vel
+                sumresult[node_x, node_y] += distribution[q, node_x, node_y]                # Sum up for the height, zeroth moment
+                sumvelx[node_x, node_y] += Cd2q9[1,q] * distribution[q, node_x, node_y]     # First moment 
+                sumvely[node_x, node_y] += Cd2q9[2,q] * distribution[q, node_x, node_y]     # Second moment
+            end
+        end
+    end
+    # Normalize the values according to standard LBM steps
+    height = sumresult
+    velocity_x = (sumvelx .+ (0.5 * forces_x))./sumresult
+    velocity_y = (sumvely .+ (0.5 * forces_y))./sumresult
+    # Return the computed values for the height velocity vector
+    return height, velocity_x, velocity_y
+end
+
 
 """
     momentsd1q3(height, velocity, distribution)
